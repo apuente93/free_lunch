03e24a1b6e821b91cb0f1936d20586f2095e9b7d
Alejandro Puente <apuente@wisc.edu>
2015-04-16 22:10:34 +0000

    Finished adding home and about pages. Root now goes to home page.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Free Lunch - Home"
   end
 
-  test "should get help" do
-    get :help
+  test "should get about" do
+    get :about
     assert_response :success
+    assert_select "title", "Free Lunch - About"
   end
 
 end
