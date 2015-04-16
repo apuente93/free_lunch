dc0fd9ed60ccb5f6a0b62c065d4bbbf8bd72e21d
Alejandro Puente <apuente@wisc.edu>
2015-04-16 22:31:56 +0000

    Title headers for home and about are more dynamic

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Free Lunch - Home"
+    assert_select "title", "Free Lunch"
   end
 
   test "should get about" do
