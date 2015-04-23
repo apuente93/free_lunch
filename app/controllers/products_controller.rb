06b1c4f1d9f6ac76824cb64f3742430f74ca1fee
Alejandro Puente <apuente@wisc.edu>
2015-04-23 19:58:09 +0000

    Created products controller with basic new/create methods.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,42 @@
+class ProductsController < ApplicationController
+  before_action :logged_in_user, only: [:new, :create]
+  before_action :admin_user,     only: [:new, :create]
+  
+  def new
+    @product = Product.new
+  end
+  
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:success] = "Product created!"
+      redirect_to root_path
+    else
+      redirect_to 'new'
+    end
+  end
+  
+  private
+
+    def product_params
+      params.require(:product).permit(:name, :category)
+    end
+    
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+      store_location
+        flash[:danger] = "Please log in as admin user."
+        redirect_to login_url
+      end
+    end
+    
+    # Confirms an admin user.
+    def admin_user
+      unless current_user.admin?
+        flash[:danger] = "Please log in as admin user."
+        redirect_to(root_url)
+      end
+    end
+  
+end
