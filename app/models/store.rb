5e6a81c4f2af1c24871496d51e8f0fd58b92bb64
Alejandro Puente <apuente@wisc.edu>
2015-04-30 21:15:11 +0000

    Updated store model to allow stores with the same name. You can have two stores with the same name, but with different addresses.

--- app/models/store.rb
+++ app/models/store.rb
@@ -8,7 +8,6 @@ class Store < ActiveRecord::Base
   has_many :order, through: :place
   has_one :user, through: :place
   
-  validates :name, presence: true, length: {maximum: 30},
-            uniqueness: {case_sensitive: false}
+  validates :name, presence: true, length: {maximum: 30}
   validates :address, presence: true, length: {maximum: 100}
 end
