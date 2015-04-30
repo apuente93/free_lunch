bb5115b52c91b03c5c2c8a41a0b60aeab02364d2
Alejandro Puente <apuente@wisc.edu>
2015-04-30 21:09:50 +0000

    Updated seed.rb to create 10 stores in Madison area.

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,6 +25,36 @@ Store.create!(name:  "Capitol Centre Market",
 Store.create!(name:  "Woodman's Food Market",
              address: "3817 Milwaukee St, Madison, WI 53714",
              image_url: "http://nikolainuthouse.com/wp-content/uploads/2013/12/woodmans.jpg")
+            
+#Creates Copps store 
+Store.create!(name:  "Copps",
+             address: "620 S Whitney Way, Madison, WI 53711",
+             image_url: "http://www.mabaensch.com/wp-content/uploads/2015/02/copps.jpg")
+
+#Creates Pick'n Save store 
+Store.create!(name:  "Pick'n Save",
+             address: "5709 US-51, McFarland, WI 53558",
+             image_url: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRy2JpY8xTffSHIVD728hnIxs0nNHIcGBVKbY1EBxqHv3WHuVxw")
+             
+#Creates Metcalfe's store 
+Store.create!(name:  "Metcalfe's Market",
+             address: "Hilldale Shopping Center, 726 N Midvale Blvd, Madison, WI 53705",
+             image_url: "http://sabraaitime.com//wp-content/uploads/2014/11/metcalfes-box.png")
+
+#Creates Trader Joe's store 
+Store.create!(name:  "Trader Joe's",
+             address: "1810 Monroe St, Madison, WI 53711",
+             image_url: "http://corneliusnews.net/wp-content/uploads/2014/10/traderjoes_logo.jpg")
+
+#Creates Copps(2) store 
+Store.create!(name:  "Copps",
+             address: "6800 Century Ave, Middleton, WI 53562",
+             image_url: "http://www.mabaensch.com/wp-content/uploads/2015/02/copps.jpg")
+
+#Creates Willy Street Co-op store 
+Store.create!(name:  "Willy Street Co-op",
+             address: "1221 Williamson St, Madison, WI 53703",
+             image_url: "http://www.progressivegrocer.com/sites/default/files/styles/article-full/public/PG%20Articles/Willy%20Street%20Co-op%20Resized.jpg?itok=2OziQe7L")
              
 =begin
 99.times do |n|
