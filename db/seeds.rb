f41c183c565514a6720a85c08e97b23f93518fb7
xwang322 <xwang322@wisc.edu>
2015-05-03 19:56:35 +0000

    we have 4 admin users, so that everyone can log in as admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,27 @@ User.create!(username:  "apuente93",
              password:              "9066278202",
              password_confirmation: "9066278202",
              admin: true)
+             
+User.create!(username:  "xwang322",
+             email: "xwang322@wisc.edu",
+             address: "4701 Sheboygan Ave, Madison, WI 53705",
+             password:              "9066383432",
+             password_confirmation: "9066383432",
+             admin: true)
+
+User.create!(username:  "bozhao",
+             email: "bqi22@wisc.edu",
+             address: "610 Eagle Height, APT G, Madison, WI 53705",
+             password:              "9068273680",
+             password_confirmation: "9068273680",
+             admin: true)
+
+User.create!(username:  "gdavies",
+             email: "gdavies@wisc.edu",
+             address: "1334 Vilas Ave, Madison, WI 53715",
+             password:              "9067177924",
+             password_confirmation: "9067177924",
+             admin: true)
 
 #Creates Fresh Madison Market store
 Store.create!(name:  "Fresh Madison Market",
